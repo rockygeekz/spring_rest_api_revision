5004b75f2140d25f1037705632242f138210391d
Rakesh s <rakesh.s552004@gmail.com>
2025-08-01 14:33:58 +0530

    added get and delete

--- src/main/java/com/rakeshs/spring_rest_api_revision/Controller/Empcontroller.java
+++ src/main/java/com/rakeshs/spring_rest_api_revision/Controller/Empcontroller.java
@@ -2,8 +2,12 @@ package com.rakeshs.spring_rest_api_revision.Controller;
 
 import com.rakeshs.spring_rest_api_revision.Entity.Employee;
 import com.rakeshs.spring_rest_api_revision.Service.Empservice;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -25,6 +29,31 @@ public class Empcontroller {
     public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
         Employee savedEmployee = empservice.postEmployee(employee);
         return ResponseEntity.ok(savedEmployee);
+    }
+
+    @GetMapping("/employee")
+    public ResponseEntity<List<Employee>> getAllEmployee() {
+        List<Employee> employees = empservice.getAllEmployees();
+        return ResponseEntity.ok(employees);
+    }
+
+    @GetMapping("/employee/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = empservice.getEmployeeById(id);
+        if(employee == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(employee);
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable Long id) {
+        try {
+            empservice.deleteEmployee(id);
+            return new ResponseEntity<>("Employee with ID " + id + " deleted successfully", HttpStatus.OK);
+        } catch (EntityNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 
 }
--- src/main/java/com/rakeshs/spring_rest_api_revision/Service/Empservice.java
+++ src/main/java/com/rakeshs/spring_rest_api_revision/Service/Empservice.java
@@ -2,8 +2,12 @@ package com.rakeshs.spring_rest_api_revision.Service;
 
 import com.rakeshs.spring_rest_api_revision.Entity.Employee;
 import com.rakeshs.spring_rest_api_revision.repository.EmployeeRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +18,21 @@ public class Empservice {
     public Employee postEmployee(Employee employee){
         return employeeRepository.save(employee);
     }
+
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id){
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+    public void deleteEmployee(Long id) {
+        if (!employeeRepository.existsById(id)) {
+            throw new EntityNotFoundException("Employee with ID " + id + " not found");
+        }
+        employeeRepository.deleteById(id);
+    }
+
 
 }
