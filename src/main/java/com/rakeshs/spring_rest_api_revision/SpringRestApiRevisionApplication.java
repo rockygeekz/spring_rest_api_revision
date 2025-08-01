293f43e5e3507b640e860d6fe63e4ef8392b1d9a
Rakesh s <rakesh.s552004@gmail.com>
2025-08-01 12:08:29 +0530

    Spring boot project initialised Ready to Start !!

---
+++ src/main/java/com/rakeshs/spring_rest_api_revision/SpringRestApiRevisionApplication.java
@@ -0,0 +1,13 @@
+package com.rakeshs.spring_rest_api_revision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiRevisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiRevisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rakeshs/spring_rest_api_revision/SpringRestApiRevisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakeshs.spring_rest_api_revision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiRevisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
